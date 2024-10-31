9732d0ee4896267cd168f21d184df1f2bc73123d
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2024-10-31 14:17:03 -0700

    CI (internal): add script to run ispctests on Windows

---
+++ .github/workflows/scripts/internal/run-ispctests.bat
@@ -0,0 +1,28 @@
+@echo on
+setlocal enabledelayedexpansion
+
+cd Scripts
+
+:: Run GenerateProjectFiles script
+call GenerateProjectFiles.bat
+
+:: Define targets and file type
+set "targets=release avx2 sse2 sse4"
+
+:: Build targets
+pushd ..\Build\ISPCTest\vs2019\msvc
+for %%t in (%targets%) do (
+    set "target=%%t"
+    msbuild ISPCTest.sln /t:clean /p:Configuration=!target!
+    msbuild ISPCTest.sln /p:Configuration=!target! /m
+    if errorlevel 1 (
+        echo "Build failed for target !target!"
+        exit /b 1
+    )
+)
+popd
+
+:: Run tests for one configuration only to save time
+pushd ..\x86_64
+ISPCTest-release.exe --benchmark_min_time=0.1 --benchmark_repetitions=1 --benchmark_out=validate.json --benchmark_out_format=json --benchmark_report_aggregates_only=true
+popd

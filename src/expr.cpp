c3645c93c7eee48d1f1dee7bbadeb894d4ff8c40
Arina Neshlyaeva <arina.neshlyaeva@intel.com>
2025-05-30 14:38:11 -0700

    Skip type checking for FunctionCallExpr if its type is dependent

--- src/expr.cpp
+++ src/expr.cpp
@@ -4320,6 +4320,9 @@ Expr *FunctionCallExpr::TypeCheck() {
             return nullptr;
         }
 
+        if (func->GetType() != nullptr && func->GetType()->IsDependent()) {
+            return this;
+        }
         funcType = CastType<FunctionType>(func->GetType());
         if (funcType == nullptr) {
             const PointerType *pt = CastType<PointerType>(func->GetType());
